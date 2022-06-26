53aafeb71652ec9d4e33c02321e46287ecaebdf0
Laercio de Souza Rios <laercio.rios13@hotmail.com>
2022-06-26 12:35:47 -0300

    :lipstick: Added icon in new task button

--- src/Components/NewTask.tsx
+++ src/Components/NewTask.tsx
@@ -1,3 +1,5 @@
+import { PlusCircle } from "phosphor-react";
+
 import styles from "./NewTask.module.css";
 
 export function NewTask() {
@@ -5,7 +7,9 @@ export function NewTask() {
     <div className={styles.newTask}>
       <form action="submit">
         <textarea name="task" placeholder="Adicione uma nova tarefa"></textarea>
-        <button type="submit">Criar</button>
+        <button type="submit">
+          Criar <PlusCircle size={20} />
+        </button>
       </form>
     </div>
   );
