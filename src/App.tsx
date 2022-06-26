7e0741b799c10b44ce5b87cc947381f193d6841f
Laercio de Souza Rios <laercio.rios13@hotmail.com>
2022-06-26 11:53:41 -0300

    :lipstick: Added styles for body

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Header } from "./Components/Header";
 
+import styles from "./App.module.css";
 import "./global.css";
 
 function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}></div>
     </div>
   );
 }
