a19ce2cbd3809b1bc6676364f82535736d3e7c88
Laercio de Souza Rios <laercio.rios13@hotmail.com>
2022-06-26 19:15:35 -0300

    :sparkles: Added count of tasks

--- src/Components/ListTasks.tsx
+++ src/Components/ListTasks.tsx
@@ -6,8 +6,25 @@ interface ListTasksProps {
 }
 
 export function ListTasks({ tasks }: ListTasksProps) {
+  const totalTasks = tasks.length;
+  const totalTasksDone = tasks.reduce((acc, task) => {
+    return task.isDone ? acc + 1 : acc;
+  }, 0);
+
   return (
     <div className={styles.listTasks}>
+      <div className={styles.headerListTasks}>
+        <strong className={styles.tasksCreated}>
+          Tarefas criadas{" "}
+          <span className={styles.totalTasks}>{totalTasks}</span>
+        </strong>
+        <strong className={styles.tasksDone}>
+          Concluídas{" "}
+          <span className={styles.totalTasks}>
+            {totalTasks > 0 ? `${totalTasksDone} de ${totalTasks}` : 0}
+          </span>
+        </strong>
+      </div>
       <ul>
         {tasks.map((task) => {
           return <li key={task.id}>{task.description}</li>;
